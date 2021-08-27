468ff075b9dcbacb7cd098d71ce88116106df47f
Alfie Edwards <alfiee@graphcore.ai>
2021-08-27 16:06:44 +0100

    Adding rnn layers
    Summary:
    TF1.15 Only
    
    Copying in non-keras rnn layers from the main repo.
    
    Updated the copyright headers and namespaces.
    
    V2 diff: D50540
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Subscribers: georgep
    
    Maniphest Tasks: T44505
    
    Differential Revision: https://phabricator.sourcevertex.net/D50529

--- ipu_tensorflow_addons/layers/__init__.py
+++ ipu_tensorflow_addons/layers/__init__.py
@@ -1,1 +1,10 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+
+from ipu_tensorflow_addons.layers.rnn_ops import PopnnLSTM
+from ipu_tensorflow_addons.layers.rnn_ops import PopnnDynamicLSTM
+from ipu_tensorflow_addons.layers.rnn_ops import PopnnGRU
+from ipu_tensorflow_addons.layers.rnn_ops import PopnnDynamicGRU
+from ipu_tensorflow_addons.layers.rnn_ops import PopnnAUGRU
+
+# Registers gradient functions for the rnn layers.
+from ipu_tensorflow_addons.layers import rnn_ops_grad
