2bfead410cb41e3eaf54af1707b417f15b5849c6
Alfie Edwards <alfiee@graphcore.ai>
2021-08-27 16:06:27 +0100

    Adding rnn layers and keras effective transformer
    Summary:
    TF2.4 Only
    
    Copying in non-keras rnn layers and keras effective transformer from the main repo.
    
    Updated the copyright headers and namespaces.
    
    V1 diff: D50529
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T44505
    
    Differential Revision: https://phabricator.sourcevertex.net/D50540

--- ipu_tensorflow_addons/keras/layers/__init__.py
+++ ipu_tensorflow_addons/keras/layers/__init__.py
@@ -1,1 +1,4 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+
+from ipu_tensorflow_addons.keras.layers.effective_transformer import EffectiveTransformer
+
--- ipu_tensorflow_addons/v1/layers/__init__.py
+++ ipu_tensorflow_addons/v1/layers/__init__.py
@@ -1,1 +1,11 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+
+from ipu_tensorflow_addons.v1.layers.rnn_ops import PopnnLSTM
+from ipu_tensorflow_addons.v1.layers.rnn_ops import PopnnDynamicLSTM
+from ipu_tensorflow_addons.v1.layers.rnn_ops import PopnnGRU
+from ipu_tensorflow_addons.v1.layers.rnn_ops import PopnnDynamicGRU
+from ipu_tensorflow_addons.v1.layers.rnn_ops import PopnnAUGRU
+
+# Registers gradient functions for the rnn layers.
+from ipu_tensorflow_addons.v1.layers import rnn_ops_grad
+
