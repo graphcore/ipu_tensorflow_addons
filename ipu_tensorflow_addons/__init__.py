e4f25ce20075712ac8bfc2b56db78f4922f36b89
Alfie Edwards <alfiee@graphcore.ai>
2021-10-13 09:37:31 +0100

    Adding descriptions to public __init__ files
    Summary:
    TF1.15 Only
    
    Adding descriptions to public (exported in the wheel) init files.
    
    V2 diff: D53345
    
    Test Plan: No code changes
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T44500
    
    Differential Revision: https://phabricator.sourcevertex.net/D53347

--- ipu_tensorflow_addons/__init__.py
+++ ipu_tensorflow_addons/__init__.py
@@ -1,1 +1,6 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
+
+"""
+A collection of addons for IPU Tensorflow
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
--- ipu_tensorflow_addons/layers/__init__.py
+++ ipu_tensorflow_addons/layers/__init__.py
@@ -1,5 +1,10 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
 
+"""
+TensorFlow layers made for IPU TensorFlow
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
 from ipu_tensorflow_addons.layers.rnn_ops import PopnnLSTM
 from ipu_tensorflow_addons.layers.rnn_ops import PopnnDynamicLSTM
 from ipu_tensorflow_addons.layers.rnn_ops import PopnnGRU
