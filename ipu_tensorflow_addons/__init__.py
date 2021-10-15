51609df5fb3d6c7b95c149f62303a437b47982da
Alfie Edwards <alfiee@graphcore.ai>
2021-10-15 09:45:02 +0100

    Checking installed TF is IPU TF in init
    Summary:
    Attempt to import the tensorflow.python.ipu namespace to test whether the installed tensorflow version is IPU tensorflow.
    It only shows on ModuleNotFound so if we get a generic ImportError (e.g. if poplar is not activated) then it will just show that.
    It still does the check even if you only import a child namespace too (e.g. from ipu_tensorflow_addons.keras import layers).
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Subscribers: georgew, georgep
    
    Maniphest Tasks: T46765
    
    Differential Revision: https://phabricator.sourcevertex.net/D53446

--- ipu_tensorflow_addons/__init__.py
+++ ipu_tensorflow_addons/__init__.py
@@ -3,3 +3,13 @@
 A collection of addons for IPU Tensorflow
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
+
+try:
+  # Test if IPU TensorFlow is available.
+  import tensorflow.python.ipu as ipu
+  # Remove the reference we just created from this namespace.
+  del ipu
+except ModuleNotFoundError:
+  raise ImportError(
+      "Failed to import IPU TensorFlow. Make sure you have IPU TensorFlow "
+      "installed, and not a different TensorFlow release.")
