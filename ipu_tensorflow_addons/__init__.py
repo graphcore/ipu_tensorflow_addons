f25fbaec0cd41f4d26ed289d168da2c89e79bf00
Alfie Edwards <alfiee@graphcore.ai>
2021-10-18 13:13:52 +0100

    TensorFlow Capitalisation
    Summary: Corrrecting TensorFlow capitalisation
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, markf
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, markf
    
    Maniphest Tasks: T47768
    
    Differential Revision: https://phabricator.sourcevertex.net/D53639

--- ipu_tensorflow_addons/__init__.py
+++ ipu_tensorflow_addons/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
 """
-A collection of addons for IPU Tensorflow
+A collection of addons for IPU TensorFlow
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
