2aaaecee7900d44c0c58a523323ee7743ad1eed1
Alfie Edwards <alfiee@graphcore.ai>
2021-10-25 09:06:28 +0100

    Updating wheel metadata
    Summary:
    TF2.4 Only
    
    Filling out metadata for the addons wheel.
    Added version number, description, and license.
    Also added LICENSE.txt
    Left author-email unspecified.
    
    The only diference from the V1 diff is the version number.
    
    V1 diff: D53579
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T47768
    
    Differential Revision: https://phabricator.sourcevertex.net/D53580

--- ipu_tensorflow_addons/tools/pip_package/setup.py
+++ ipu_tensorflow_addons/tools/pip_package/setup.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
 
 import os
+import sys
 import re
 from setuptools import setup
 
@@ -17,13 +18,21 @@ def find_packages(pattern=r".*"):
   return packages
 
 
+if "TF_VERSION" not in os.environ:
+  sys.exit(
+      "ERROR: TF_VERSION is not set. Please set TF_VERSION to the TensorFlow "
+      "version the wheel is being built.")
+tf_version = os.environ["TF_VERSION"]
+
 setup(
     name='ipu_tensorflow_addons',
-    version='0.0.0',
+    description='A collection of addons for IPU TensorFlow',
+    version=tf_version,
     python_requires='>=3.6',
     url='https://www.graphcore.ai/',
     author='Graphcore Ltd.',
     # Contained modules and scripts.
     packages=find_packages(r"ipu_tensorflow_addons\.?.*"),
-    license='Apache 2.0',
+    license='MIT',
+    install_requires=[f"tensorflow=={tf_version}"],
 )
