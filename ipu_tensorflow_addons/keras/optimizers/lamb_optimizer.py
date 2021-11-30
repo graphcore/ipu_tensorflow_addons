c5ebd844911724d5e7e5512d1bb4b9c36c8dc8ee
Alfie Edwards <alfiee@graphcore.ai>
2021-11-30 11:29:06 +0000

    Using Apache 2.0 license
    Summary:
    Using Apache 2.0 license, and retaining original copyright attribution on files adapted from the Graphcore fork of TensorFlow.
    
    TF2.4 Only
    
    V1 diff: D56550
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, grahamh, georgew
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, grahamh, georgew
    
    Subscribers: georgew
    
    Maniphest Tasks: T50302
    
    Differential Revision: https://phabricator.sourcevertex.net/D56527

--- ipu_tensorflow_addons/keras/optimizers/lamb_optimizer.py
+++ ipu_tensorflow_addons/keras/optimizers/lamb_optimizer.py
@@ -1,7 +1,4 @@
 # Copyright (c) 2021 Graphcore Ltd. All rights reserved.
-# ==============================================================================
-# Parts of this code are derived from TensorFlow.
-#
 # Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# This file has been modified by Graphcore Ltd.
 # ==============================================================================
 """Layer-wise Adaptive Moments (LAMB) optimizer."""
 
