b5b55a892cc34c260f12a78a515c4e4e65d90301
Alfie Edwards <alfiee@graphcore.ai>
2021-11-30 13:08:20 +0000

    TensorFlow capitalization
    Summary:
    Correcting the capitalisation of TensorFlow in the root init file.
    The capitalisation is already correct on the v1 branch.
    
    TF2.4 Only
    
    Reviewers: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Reviewed By: #tensorflow, #framework_ip_review_-_any_oss_or_third-party_code_use_has_been_approved, georgep
    
    Maniphest Tasks: T50302
    
    Differential Revision: https://phabricator.sourcevertex.net/D56743

--- ipu_tensorflow_addons/__init__.py
+++ ipu_tensorflow_addons/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 """
-A collection of addons for IPU Tensorflow
+A collection of addons for IPU TensorFlow
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
